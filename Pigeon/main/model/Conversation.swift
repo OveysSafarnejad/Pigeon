be3fdb51b956baeaebaf2798685aadbe172fed98
Oveys Safarnejad <safarnejad@oveyss-mac.local>
2019-02-21 02:13:06 -0800

    conversation mapping

--- Pigeon/main/model/Conversation.swift
+++ Pigeon/main/model/Conversation.swift
@@ -26,39 +26,168 @@ enum LastMessageState : String {
 
 class ConversationMapping {
 
-    var conversationId : String!
-    var conversationType : ConversationType!
-    var isMuted : Bool!
-    var isPinned : Bool!
-    var lastSeenDate : String!
-    var lastUpdate : String!
-    var lastMessageId : String!
-    var noMoreMessages : Bool!
-    var unreadCount : Int!
-    var hiddenConversation : Bool!
-    var lastMessageState : LastMessageState!
-    var draftMessage : String!
-    var messages : [Message]!
-    var contact : Contact!
-
-
-
-    init(conversationId : String , conversationType : ConversationType , isMuted : Bool , isPinned : Bool , lastSeenDate : String , lastUpdate : String , lastMessageId : String , noMoreMessages : Bool , unreadCount : Int , hiddenConversation : Bool , lastMessageState : LastMessageState , draftMessage : String , messages : [Message] , contact : Contact) {
-        
-        self.conversationId = conversationId
-        self.conversationType = conversationType
-        self.isMuted = isMuted
-        self.isPinned = isPinned
-        self.lastSeenDate = lastSeenDate
-        self.lastUpdate = lastUpdate
-        self.lastMessageId = lastMessageId
-        self.noMoreMessages = noMoreMessages
-        self.unreadCount = unreadCount
-        self.hiddenConversation = hiddenConversation
-        self.lastMessageState = lastMessageState
-        self.draftMessage = draftMessage
-        self.messages = messages
-        self.contact = contact
+    private var _conversationId : String!
+    private var _conversationType : ConversationType!
+    private var _isMuted : Bool!
+    private var _isPinned : Bool!
+    private var _lastSeenDate : String!
+    private var _lastUpdate : String!
+    private var _lastMessageId : String!
+    private var _noMoreMessages : Bool!
+    private var _unreadCount : Int!
+    private var _hiddenConversation : Bool!
+    private var _lastMessageState : LastMessageState!
+    private var _draftMessage : String!
+    private var _messages : [Message]!
+    private var _contact : ContactMapping!
+    
+    var conversationId : String {
+        get {
+            return _conversationId
+        }
+        set(id) {
+            _conversationId = id
+        }
     }
+    
+    var conversationType : ConversationType {
+        get {
+            return _conversationType
+        }
+        set(conversationType) {
+            _conversationType = conversationType
+        }
+    }
+    
+    var isMuted : Bool {
+        get {
+            return _isMuted
+        }
+        set(isMuted) {
+            _isMuted = isMuted
+        }
+    }
+    
+    var isPinned : Bool {
+        get {
+            return _isPinned
+        }
+        set(isPinned) {
+            _isPinned = isPinned
+        }
+    }
+    
+    var lastSeenDate : String {
+        get {
+            return _lastSeenDate
+        }
+        set(lastSeenDate) {
+            _lastSeenDate = lastSeenDate
+        }
+    }
+    
+    var lastUpdate : String {
+        get {
+            return _lastUpdate
+        }
+        set(lastUpdate) {
+            _lastUpdate = lastUpdate
+        }
+    }
+    
+    var lastMessageId : String {
+        get {
+            return _lastMessageId
+        }
+        set(lastMessageId) {
+            _lastMessageId = lastMessageId
+        }
+    }
+    
+    var noMoreMessage : Bool {
+        get {
+            return _noMoreMessages
+        }
+        set(noMoreMessage) {
+            _noMoreMessages = noMoreMessage
+        }
+    }
+    
+    var unreadCount : Int {
+        get {
+            return _unreadCount
+        }
+        set(id) {
+            _unreadCount = unreadCount
+        }
+    }
+    
+    var hiddenConversation : Bool {
+        get {
+            return _hiddenConversation
+        }
+        set(hiddenConversation) {
+            _hiddenConversation = hiddenConversation
+        }
+    }
+    
+    var lastMessageState : LastMessageState {
+        get {
+            return _lastMessageState
+        }
+        set(lastMessageState) {
+            _lastMessageState = lastMessageState
+        }
+    }
+    
+    var draftMessage : String {
+        get {
+            return _draftMessage
+        }
+        set(draftMessage) {
+            _draftMessage = draftMessage
+        }
+    }
+    
+    var messages : [Message] {
+        get {
+            return _messages
+        }
+        set(messages) {
+            _messages = messages
+        }
+    }
+    
+    var contact : ContactMapping {
+        get {
+            return _contact
+        }
+        set(contact) {
+            _contact = contact
+        }
+    }
+    
+    
+    
+    
+    
+    init(conversationId : String , conversationType : ConversationType , isMuted : Bool , isPinned : Bool , lastSeenDate : String , lastUpdate : String , lastMessageId : String , noMoreMessages : Bool , unreadCount : Int , hiddenConversation : Bool , lastMessageState : LastMessageState , draftMessage : String , messages : [Message] , contact : ContactMapping) {
+        
+        self._conversationId = conversationId
+        self._conversationType = conversationType
+        self._isMuted = isMuted
+        self._isPinned = isPinned
+        self._lastSeenDate = lastSeenDate
+        self._lastUpdate = lastUpdate
+        self._lastMessageId = lastMessageId
+        self._noMoreMessages = noMoreMessages
+        self._unreadCount = unreadCount
+        self._hiddenConversation = hiddenConversation
+        self._lastMessageState = lastMessageState
+        self._draftMessage = draftMessage
+        self._messages = messages
+        self._contact = contact
+    }
+
 
 }
